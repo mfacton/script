b9d93fd599e8da4b8b2ce82b705bb18e091c1492
Miguel Flores-Acton <mfacton1@gmail.com>
2025-02-06 17:08:37 -0500

    Add automatic cd

--- shell_conf.sh
+++ shell_conf.sh
@@ -6,7 +6,7 @@ fi
 # aliases
 alias mkcd='_mkcd() { mkdir -p "$1" && cd "$1"; }; _mkcd'
 alias conm="conda activate mfact"
-alias gstat="conda activate mfact && gsync status"
+alias gstat="conda activate mfact && cd ~/Projects && gsync status"
 
 # scripts
 alias awake=$USER_SCRIPT_DIR/awake.sh
