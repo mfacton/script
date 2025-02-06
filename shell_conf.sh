02ba94d948d18b6080acee7b839fbc1067d14455
Miguel Flores-Acton <mfacton1@gmail.com>
2025-02-06 17:06:17 -0500

    Add gstat

--- shell_conf.sh
+++ shell_conf.sh
@@ -6,6 +6,7 @@ fi
 # aliases
 alias mkcd='_mkcd() { mkdir -p "$1" && cd "$1"; }; _mkcd'
 alias conm="conda activate mfact"
+alias gstat="conda activate mfact && gsync status"
 
 # scripts
 alias awake=$USER_SCRIPT_DIR/awake.sh
