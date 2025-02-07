12f5b93013bd3a418f330a7c3012c96c9a5c244c
Miguel Flores-Acton <mfacton1@gmail.com>
2025-02-07 16:21:06 -0500

    Update installer

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 FOLDER_PATH="$(pwd)"
 SCRIPT_PATH="$FOLDER_PATH/shell_conf.sh"
 
-if ! grep -Fxq "source $SCRIPT_PATH" ~/.bashrc; then
+if ! grep -Fxq "source \"\$USER_SCRIPT_DIR/shell_conf.sh\"" ~/.bashrc; then
     echo "export USER_SCRIPT_DIR=$FOLDER_PATH" >> ~/.bashrc
-    echo "source $SCRIPT_PATH" >> ~/.bashrc
+    echo "source \"\$USER_SCRIPT_DIR/shell_conf.sh\"" >> ~/.bashrc
 fi
